2d30e56bede29db1262b677cc879353a580fbfda
Andrew S. Hernandez <ash.roz0n04@gmail.com>
2024-09-12 13:57:27 -0400

    Meshes assignment done

---
+++ A3 - Meshes/Game.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "Mesh.h"
+
+#include <d3d11.h>
+#include <wrl/client.h>
+#include <DirectXMath.h>
+
+using namespace std;
+
+class Game
+{
+public:
+	// Basic OOP setup
+	Game() = default;
+	~Game();
+	Game(const Game&) = delete; // Remove copy constructor
+	Game& operator=(const Game&) = delete; // Remove copy-assignment operator
+
+	// Primary functions
+	void Initialize();
+	void Update(float deltaTime, float totalTime);
+	void Draw(float deltaTime, float totalTime);
+	void OnResize();
+
+
+private:
+	int number;
+	float color[4] = { 0.2f, 0.0f, 0.2f, 0.0f };
+	bool isDemoVisible = true;
+
+	// Initialization helper methods - feel free to customize, combine, remove, etc.
+	void LoadShaders();
+	void CreateGeometry();
+	void UpdateImGui(float deltaTime);
+	void BuildUI();
+
+	// Note the usage of ComPtr below
+	//  - This is a smart pointer for objects that abide by the
+	//     Component Object Model, which DirectX objects do
+	//  - More info here: https://github.com/Microsoft/DirectXTK/wiki/ComPtr
+
+	// Buffers to hold actual geometry data
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> indexBuffer;
+
+	// Shaders and shader-related constructs
+	Microsoft::WRL::ComPtr<ID3D11PixelShader> pixelShader;
+	Microsoft::WRL::ComPtr<ID3D11VertexShader> vertexShader;
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> inputLayout;
+
+	//Meshes
+	Mesh* starterTriangle;
+	Mesh* diamond;
+	Mesh* hexagon;
+};
+
